e817e21d18359e1cc1d630a7dfb8961f448d4a2f
Alexandrboch <al_boch21@mail.ru>
2020-11-03 14:59:23 +0300

    changes in input

--- solar_project/solar_input.py
+++ solar_project/solar_input.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # license: GPLv3
 
-from solar_objects import Star, Planet
+from solar_objects import SkyObject
 from solar_vis import DrawableObject
 
 def read_space_objects_data_from_file(input_filename):
@@ -18,55 +18,16 @@ def read_space_objects_data_from_file(input_filename):
         for line in input_file:
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
-
-            object_type = line.split()[0].lower()
-            if object_type == "star":
-                star = Star()
-                parse_star_parameters(line, star)
-                objects.append(star)
-            elif object_type == "planet":
-                planet = Planet()
-                parse_planet_parameters(line, planet)
-                objects.append(planet)
-            else:
-                print("Unknown space object")
+            skyobject = SkyObject()
+            parse_sky_object_parameters(line, skyobject)
+            objects.append(skyobject)
 
     return [DrawableObject(obj) for obj in objects]
 
 
-def parse_star_parameters(line, star):
-    """Считывает данные о звезде из строки.
-
-    Входная строка должна иметь слеюущий формат:
-
-    Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
-    Здесь (x, y) — координаты зведы, (Vx, Vy) — скорость.
-
-    Пример строки:
-
-    Star 10 red 1000 1 2 3 4
-
-    Параметры:
-
-    **line** — строка с описание звезды.
-
-    **star** — объект звезды.
-    """
-
-    tokens = line.split()
-    assert(tokens[0].lower() == 'star')
-    assert(len(tokens) == 8)
-    star.R = int(tokens[1])
-    star.color = tokens[2]
-    star.m = float(tokens[3])
-    star.x = float(tokens[4])
-    star.y = float(tokens[5])
-    star.Vx = float(tokens[6])
-    star.Vy = float(tokens[7])
 
 
-def parse_planet_parameters(line, planet):
+def parse_sky_object_parameters(line, sky_object):
     """Считывает данные о планете из строки.
     Входная строка должна иметь слеюущий формат:
 
@@ -82,18 +43,17 @@ def parse_planet_parameters(line, planet):
 
     **line** — строка с описание планеты.
 
-    **planet** — объект планеты.
+    **sky_object** — объект планеты.
     """
     tokens = line.split()
-    assert(tokens[0].lower() == 'planet')
     assert(len(tokens) == 8)
-    planet.R = int(tokens[1])
-    planet.color = tokens[2]
-    planet.m = float(tokens[3])
-    planet.x = float(tokens[4])
-    planet.y = float(tokens[5])
-    planet.Vx = float(tokens[6])
-    planet.Vy = float(tokens[7])
+    sky_object.R = int(tokens[1])
+    sky_object.color = tokens[2]
+    sky_object.m = float(tokens[3])
+    sky_object.x = float(tokens[4])
+    sky_object.y = float(tokens[5])
+    sky_object.Vx = float(tokens[6])
+    sky_object.Vy = float(tokens[7])
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
